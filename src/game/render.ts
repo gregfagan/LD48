25aa2794b51ea12149bdfe3d461711cc322755bb
gregfagan <gregfagan@gmail.com>
2021-04-24 13:56:11 -0500

    nevermind, 16x16 is fine...

--- src/game/render.ts
+++ src/game/render.ts
@@ -18,10 +18,7 @@ import { gui as baseGui } from './util';
 
 const gui = baseGui.addFolder('colors');
 
-const size: Vec2 = [width, height];
 const textureSize: Vec2 = [16, 16];
-const aspect = width / height;
-const textureRatio: Vec2 = vec2.div(size, textureSize);
 
 type RenderBoard = Grid<Vec4, typeof textureSize[0], typeof textureSize[1]>;
 
@@ -58,51 +55,17 @@ export const render = (regl: Regl) => {
     ${{
       uniforms: { board: texture },
       depth: { enable: false },
-      scissor: { enable: false },
     }}
 
     uniform sampler2D board;
-    #define aspect ${aspect}
-    #define minTR min(u_textureRatio.x, u_textureRatio.y)
-
-    vec2 texST() {
-      vec2 size = ${uniform(() => size)};
-      vec2 vr = viewport / min(viewport.x, viewport.y);
-
-      float offset = (1. - min(u_textureRatio.x, u_textureRatio.y)) / 2.;
-
-      vec2 p = uv;
-      p = p * vr;
-
-      offset = max(vr.x, vr.y) - 1./aspect;
-      p = p - ((viewport.x > viewport.y)
-        ? vec2((vr.x - aspect) / 2., 0)
-        : vec2(0, (vr.y - 1./aspect)/2.));
-
-      return p;
-    }
-
-    vec2 pBoard() {
-      vec2 tr = ${uniform(() => textureRatio, 'u_textureRatio')};
-      vec2 p = texST();
-      p = p * ((viewport.x < viewport.y) ? u_textureRatio.x : u_textureRatio.y);
-      return p;
-    }
-
-
-    float inBounds() {
-      vec2 p = pBoard();
-      vec2 bounds = vec2(1);
-      bounds = step(vec2(0), p) * step(p, u_textureRatio);
-      return bounds.x * bounds.y;
-    }
-
+  
     vec4 colorBoard() {
-      return vec4(texture2D(board, pBoard()).xyz, inBounds());
+      vec2 p = st() / 2. + 0.5;
+      return vec4(texture2D(board, p * vec2(1, -1)).xyz, 1.);
     }
 
     void main() {
-      gl_FragColor = vec4(mix(colorBoard().xyz, vec3(0.5), 1.-inBounds()), 1);
+      gl_FragColor = colorBoard();
     }
   `;
 
