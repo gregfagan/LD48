da53493a90116fadd02c544df6fda9e1e0deb113
gregfagan <gregfagan@gmail.com>
2021-04-24 13:23:49 -0500

    aspect fit board (kind of)
    this is very bad. I am ashamed. it doesn't really work when the aspect is close
    to square. Should be simple math...
    
    :(

--- src/game/render.ts
+++ src/game/render.ts
@@ -1,24 +1,33 @@
 import { flow } from 'fp-ts/lib/function';
-import { Regl, Vec3, Vec4 } from 'regl';
+import { DefaultContext, Regl, Vec2, Vec3, Vec4 } from 'regl';
 import { toGLColor } from '../lib/gl/color';
 import { quad } from '../lib/gl/config/quad';
 import { sdf } from '../lib/gl/config/sdf';
-import { glsl } from '../lib/gl/regl';
+import { glsl, uniform } from '../lib/gl/regl';
+import { vec2 } from '../lib/math';
 import { Stream } from '../lib/stream';
-import { GameBoard, Grid, isTetronimo, Tetronimo } from './board';
+import {
+  GameBoard,
+  Grid,
+  height,
+  isTetronimo,
+  Tetronimo,
+  width,
+} from './board';
 import { gui as baseGui } from './util';
 
 const gui = baseGui.addFolder('colors');
-const textureWidth = 16;
-const textureHeight = 16;
 
-type RenderBoard = Grid<Vec4, typeof textureWidth, typeof textureHeight>;
+const size: Vec2 = [width, height];
+const textureSize: Vec2 = [16, 16];
+const aspect = width / height;
+const textureRatio: Vec2 = vec2.div(size, textureSize);
+
+type RenderBoard = Grid<Vec4, typeof textureSize[0], typeof textureSize[1]>;
 
 const colorize = (board: GameBoard): RenderBoard =>
   board.map(row =>
-    row.map(cell =>
-      isTetronimo(cell) ? [...colors[cell]?.(), 1] : [0, 0, 0, 0]
-    )
+    row.map(cell => (isTetronimo(cell) ? [...colors[cell](), 1] : [0, 0, 0, 0]))
   ) as RenderBoard;
 
 const colors: Record<Tetronimo, Stream<Vec3>> = {
@@ -37,8 +46,8 @@ export const render = (regl: Regl) => {
     mag: 'nearest',
     format: 'rgba',
     type: 'float32',
-    width: textureWidth,
-    height: textureHeight,
+    shape: textureSize,
+    wrap: 'repeat',
   });
 
   const update = flow(colorize, data => texture.subimage(data));
@@ -46,18 +55,54 @@ export const render = (regl: Regl) => {
   const draw = glsl`
     ${quad}
     ${sdf}
+    ${{
+      uniforms: { board: texture },
+      depth: { enable: false },
+      scissor: { enable: false },
+    }}
 
-    ${{ uniforms: { board: texture }, depth: { enable: false } }}
     uniform sampler2D board;
+    #define aspect ${aspect}
+    #define minTR min(u_textureRatio.x, u_textureRatio.y)
+
+    vec2 texST() {
+      vec2 size = ${uniform(() => size)};
+      vec2 vr = viewport / min(viewport.x, viewport.y);
+
+      float offset = (1. - min(u_textureRatio.x, u_textureRatio.y)) / 2.;
+
+      vec2 p = uv;
+      p = p * vr;
+
+      offset = max(vr.x, vr.y) - 1./aspect;
+      p = p - ((viewport.x > viewport.y)
+        ? vec2((vr.x - aspect) / 2., 0)
+        : vec2(0, (vr.y - 1./aspect)/2.));
+
+      return p;
+    }
+
+    vec2 pBoard() {
+      vec2 tr = ${uniform(() => textureRatio, 'u_textureRatio')};
+      vec2 p = texST();
+      p = p * ((viewport.x < viewport.y) ? u_textureRatio.x : u_textureRatio.y);
+      return p;
+    }
+
+
+    float inBounds() {
+      vec2 p = pBoard();
+      vec2 bounds = vec2(1);
+      bounds = step(vec2(0), p) * step(p, u_textureRatio);
+      return bounds.x * bounds.y;
+    }
 
     vec4 colorBoard() {
-      // texture is vertically flipped from the 2D array
-      vec2 p = st() * vec2(1., -1.) / 2. + 0.5;
-      return texture2D(board, p);
+      return vec4(texture2D(board, pBoard()).xyz, inBounds());
     }
 
     void main() {
-      gl_FragColor = vec4(colorBoard().xyz, 1.);
+      gl_FragColor = vec4(mix(colorBoard().xyz, vec3(0.5), 1.-inBounds()), 1);
     }
   `;
 
