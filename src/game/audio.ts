a61de1a78263af73fbccbbf6dcb946befb680884
J. Kyle Fagan <jkyle@causethought.com>
2021-04-24 20:52:27 -0500

    shrug

--- src/game/audio.ts
+++ src/game/audio.ts
@@ -5,12 +5,10 @@ const gui = baseGui.addFolder('audio');
 export const BPM = gui.auto(60, 'BPM');
 
 export const start = () => Tone.start();
+const synth = new Tone.Synth().toDestination();
 
 //play a middle 'C' for the duration of an 8th note
 export const trigger = (beat: number) => {
-  const synth = new Tone.Synth().toDestination();
   const now = Tone.now();
-  // trigger the attack immediately
-
   synth.triggerAttackRelease(beat % 8 === 0 ? 'C5' : 'C4', '8n');
 };
