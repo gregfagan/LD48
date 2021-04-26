4127e9d9605eeb0e105cba0b261e05c3e249cbb7
J. Kyle Fagan <jkyle@causethought.com>
2021-04-25 19:56:38 -0500

    Fix timing.

--- src/game/audio.ts
+++ src/game/audio.ts
@@ -30,22 +30,17 @@ stream.on(pauseState => {
   }
 }, pause);
 
-export const start = async () => {
-  return Tone.start();
-};
-
 const drumGain = addMixer(gui, 'Drums', 0.4);
 const distortion = new Tone.Distortion(4.2).connect(drumGain);
 const drumSynth = new Tone.MembraneSynth().connect(distortion);
 const drumLoop = new Tone.Sequence(
   (time, note) => {
-    drumSynth.triggerAttackRelease(note, '4n');
+    drumSynth.triggerAttackRelease(note, '4n', time);
   },
   ['C1', 'C1', 'C1', 'C1', 'C1', 'C1', 'C1', ['C1', 'C1', 'C1']]
 );
 drumLoop.loop = true;
 drumLoop.playbackRate = 0.5;
-drumLoop.start(0);
 
 // const patterns = [
 //   ['E3', 'Gb3', 'A4', 'Gb4', 'A4', 'Gb3', 'B4', 'B3'],
@@ -69,13 +64,12 @@ const delayLFO = new Tone.LFO('4m', 0, 1).connect(arpDelay.wet);
 const arp = generateBassSynth().connect(arpDelay);
 const arpPattern = new Tone.Pattern(
   (time, note) => {
-    arp.triggerAttackRelease(note, '8n');
+    arp.triggerAttackRelease(note, '8n', time);
   },
   sample(patterns),
   'up'
 );
 arpPattern.playbackRate = 2;
-arpPattern.start(0);
 
 Tone.Transport.scheduleRepeat(() => {
   arpPattern.values = sample(patterns);
@@ -85,12 +79,18 @@ const bass = generateBassSynth();
 
 const bassPattern = new Tone.Pattern(
   (time, note) => {
-    bass.triggerAttackRelease(note, '2n');
+    bass.triggerAttackRelease(note, '2n', time);
   },
   generatePatterns(scale, 1, 2, 4),
   'upDown'
 );
 bassPattern.playbackRate = 0.5;
-bassPattern.start(0);
+
+export const start = async () => {
+  Tone.start();
+  drumLoop.start(0);
+  bassPattern.start(0);
+  arpPattern.start(0);
+};
 
 //['E2', 'Gb1', 'A2', 'Gb2'],
