05fbd24bb88c2cf2164facfff54437fc425d52b8
Greg Fagan <gregfagan@gmail.com>
2021-09-06 15:07:15 -0500

    auto renderScale for performance

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { clock, gui } from './game/util';
 import { draw } from './game';
 import { keypress } from './lib/stream/dom';
 import { resize } from './game/ui';
+import { createRenderScale } from './lib/gl/renderScale';
 
 const canvas = document.getElementById('game') as HTMLCanvasElement;
 const regl = REGL({ canvas });
@@ -20,18 +21,51 @@ stream.on(
       gui.show();
     } else gui.hide();
   },
-  stream.scan(state => !state, false, keypress('t'))
+  stream.scan(state => !state, true, keypress('t'))
 );
 
-const render = regl(draw);
+const { renderer, mode, scale } = createRenderScale(regl, gui, draw);
+scale(1);
+
+let frames = 0;
+let lastTime = clock();
+const fps = stream<number>();
+const tick = () => {
+  frames++;
+  const time = clock();
+
+  if (time > lastTime + 1000) {
+    fps((frames * 1000) / (time - lastTime));
+    lastTime = time;
+    frames = 0;
+  }
+};
+
+const target = 57;
+stream.on(frameRate => {
+  let error = (target - frameRate) / target;
+  if (error > 1) error = Math.pow(error, 16);
+  console.log('scale error', error);
+  // if (frameRate < 58) {
+  // scale(scale() * 0.9);
+  // } else if (frameRate > 60 && scale() < 1) {
+  scale(Math.max(0.25, Math.min(1, scale() - error)));
+  // }
+  console.log(
+    `performance: ${fps().toFixed(2)} fps ${scale().toFixed(3)} scale`
+  );
+}, fps);
 
 stream.on(() => {
   regl.poll();
+  tick();
+  const render = renderer();
   render();
   stats.update();
 }, clock);
 
 const ro = new ResizeObserver(([entry]) => {
   resize(entry.contentRect);
+  mode('linear');
 });
 ro.observe(document.body);
--- src/lib/gl/renderScale.ts
+++ src/lib/gl/renderScale.ts
@@ -18,58 +18,110 @@ export const createRenderScale = (
     'linear',
     'nearest',
   ]);
-  const textureSize = gui.auto(8, 'textureSize', 3, 11, 1);
+  const scale = gui.auto(0.5, 'scale', 0, 1);
   const renderer = stream.combine(
     (mode, size) => {
       const currentMode = mode();
-      if (currentMode === 'none') {
-        console.dir(textureSize.controller);
-        setControllerVisible(textureSize.controller, false);
-        return regl(config);
-      }
-      setControllerVisible(textureSize.controller, true);
-      return renderScale(config, 2 ** size(), currentMode)(regl).render;
+      setControllerVisible(scale.controller, currentMode !== 'none');
+      return currentMode === 'none'
+        ? regl(config)
+        : renderScale(config, size(), currentMode)(regl).render;
     },
-    [mode, textureSize]
+    [mode, scale]
   );
 
-  return renderer;
+  return { renderer, mode, scale };
 };
 
-export const renderScale = (
-  config: REGL.DrawConfig,
-  textureSize: number,
-  filter: REGL.TextureMagFilterType = 'nearest'
-) => (regl: REGL.Regl) => {
-  const tex = regl.texture({
-    min: 'linear',
-    mag: filter,
-    radius: textureSize,
-    wrap: 'repeat',
-  });
+export const renderScale =
+  (
+    config: REGL.DrawConfig,
+    scale: number = 0.5,
+    filter: REGL.TextureMagFilterType = 'nearest'
+  ) =>
+  (regl: REGL.Regl) => {
+    // allocate a texture big enough to hold the drawing buffer
+    const screenSize = {
+      width: regl._gl.drawingBufferWidth,
+      height: regl._gl.drawingBufferHeight,
+    };
 
-  const fbo = regl.framebuffer({
-    depth: false,
-    color: tex,
-  });
+    const texSize = {
+      width: 2 ** smallestPowerOf2(screenSize.width),
+      height: 2 ** smallestPowerOf2(screenSize.height),
+    };
 
-  const renderToTexture = regl({ framebuffer: fbo, ...config });
-  const renderToScreen = regl(glsl`
+    const scaledSize = {
+      width: screenSize.width * scale,
+      height: screenSize.height * scale,
+    };
+
+    // console.log('screen ', screenSize);
+    // console.log('tex    ', texSize);
+    // console.log('scaled ', scaledSize);
+
+    const tex = regl.texture({
+      ...texSize,
+      min: 'linear',
+      mag: filter,
+      wrap: 'repeat',
+    });
+
+    const fbo = regl.framebuffer({
+      depth: false,
+      color: tex,
+    });
+
+    const renderToTexture = regl({
+      ...config,
+      framebuffer: fbo,
+      scissor: {
+        enable: true,
+        box: ({ viewportWidth, viewportHeight }) => {
+          // console.log('box', viewportWidth, viewportHeight);
+          // box: {
+          return {
+            x: 0,
+            y: 0,
+            ...scaledSize,
+          };
+        },
+      },
+      uniforms: {
+        ...config.uniforms,
+        viewport: [scaledSize.width, scaledSize.height],
+      },
+    });
+    const renderToScreen = regl(glsl`
     ${quad}
-    ${{ uniforms: { source: fbo } }}
+    ${{
+      uniforms: {
+        source: fbo,
+        viewport: [texSize.width, texSize.height],
+      },
+    }}
     uniform sampler2D source;
     void main() {
-      gl_FragColor = vec4(texture2D(source, st() / 2. + 0.5).xyz, 1.);
+      gl_FragColor = vec4(
+        texture2D(
+          source,
+          (gl_FragCoord.xy * ${scale.toFixed(4)}) / min(viewport.x, viewport.y)
+        ).xyz,
+        1.
+      );
     }
   `);
 
-  return {
-    renderToTexture,
-    renderToScreen,
-    texture: fbo,
-    render: () => {
-      renderToTexture();
-      renderToScreen();
-    },
+    return {
+      renderToTexture,
+      renderToScreen,
+      texture: fbo,
+      render: () => {
+        renderToTexture();
+        renderToScreen();
+      },
+    };
   };
-};
+
+const smallestPowerOf2 = (length: number): number =>
+  Math.ceil(Math.log2(length));
